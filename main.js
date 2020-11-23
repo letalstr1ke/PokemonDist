c2bb5b251902a5abdccede10b66e30c33d0e49ad
infernalevp <zozulyaevgeny1995@gmail.com>
2020-11-23 23:51:09 +0300

    homework-4
    Сначала подумал что было бы логично положить метод cast: function () {} в объект abilities у персонажа, однако понял что при использовании его как обработчки события клика на копке this будет - сама кнопка, реализовал через замыкание в отдельной функции
    
    Но остался вопрос как правильно понимать где именно замыкаться, все таки как-то интуитивно хочется сделать метод именно внутрри объекта персонажа, или какой-то общий контроллер боя...
    
    Не подскажете что можно почитать на эту тему, чтобы уложить у себя в голове

--- main.js
+++ main.js
@@ -10,11 +10,13 @@ const character = {
   abilities: [
     {
       name: 'Thunder Jolt',
-      damage: random(15)
+      damage: random(15),
+      сharges: 6,
     },
     {
       name: 'Pika Papow',
-      damage: random(25)
+      damage: random(25),
+      сharges: 4,
     }
   ]
 }
@@ -80,10 +82,16 @@ function initCharakterAbilities(person) {
     abilityButton.classList.add('button');
     abilityButton.innerText = person.abilities[i].name;
 
-    abilityButton.addEventListener('click', function(){
+    const clickHandler = clicksCount(person.abilities[i].сharges, person.abilities[i].name);
+    
+    abilityButton.addEventListener('click', function() {
 
-      changeHP(random(person.abilities[i].damage), enemy);
-
+      if(!clickHandler()){
+        this.disabled = true;
+        return;
+      }
+      enemy.changeHP(random(person.abilities[i].damage));
+      
     });  
 
     $abilities.appendChild(abilityButton);
@@ -96,6 +104,40 @@ function disableAllAbilities() {
 
   for(let i = 0; i < $buttons.length; i++){
     $buttons[i].disabled = true;
+  }
+
+}
+
+function generatorLog(firstPerson, secondPerson, count){
+  const logs = [
+    `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. -${count} [${firstPerson.hp.current}]`,
+    `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику -${count} [${firstPerson.hp.current}]`,
+  ];
+
+  const $p = document.createElement('p');
+  $p.innerText = logs[random(logs.length) -1];
+  $logger.insertBefore($p, $logger.children[0]);
+
+}
+
+function clicksCount(counter, name) {
+  
+  return function () {
+    
+    const $p = document.createElement('p');
+    $p.style = 'color:red;';
+    $p.innerText = `Осталось ${--counter} зарядов у способности ${name}`; 
+    $logger.insertBefore($p, $logger.children[0]);
+
+    return counter > 0 ? true : false;
   }
 
 }
