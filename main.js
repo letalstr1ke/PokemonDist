b968ff7fc85c62a9479dc01a630892e7265183e7
infernalevp <zozulyaevgeny1995@gmail.com>
2020-11-26 20:16:25 +0300

    homework-5
    Рефакторинг на модули

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
+import Pokemon from "./js/pokemon.js";
+import random from "./js/utils.js";
+
 const $abilities = document.querySelector('#abilities');
 const $logger = document.querySelector('#logs');
 
-const character = {
+const player1 = new Pokemon({
   name: 'Pikachu',
-  defaultHP: 100,
-  damageHP: 100,
-  hp: {
-    current: 100,
-    total: 100,
-  },
-  elHP: document.getElementById('health-character'),
-  elProgressbar: document.getElementById('progressbar-character'),
+  type: 'electric',
+  hp: 500,
+  selectors: 'character',
   abilities: [
     {
       name: 'Thunder Jolt',
@@ -23,63 +21,17 @@ const character = {
       charges: 4,
     }
   ],
-  renderHP: renderHP,
-  renderHPLife: renderHPLife,
-  renderHPProgressbar: renderHPProgressbar,
-  changeHP: changeHP,
-}
+});
 
-const enemy = {
+const player2 = new Pokemon({
   name: 'Charmander',
-  defaultHP: 100,
-  damageHP: 100,
-  hp: {
-    current: 150,
-    total: 150,
-  },
-  elHP: document.getElementById('health-enemy'),
-  elProgressbar: document.getElementById('progressbar-enemy'),
-  renderHP: renderHP,
-  renderHPLife: renderHPLife,
-  renderHPProgressbar: renderHPProgressbar,
-  changeHP: changeHP,
-}
+  type: 'fire',
+  hp: 400,
+  selectors: 'enemy',
+});
 
 function init() {
-  character.renderHP();
-  enemy.renderHP();
-  initCharakterAbilities(character);
-}
-
-function renderHP() {
-  this.renderHPLife();
-  this.renderHPProgressbar();
-}
-
-function renderHPLife() {
-  this.elHP.innerText = this.hp.current + ' / ' + this.hp.total;
-}
-
-function renderHPProgressbar() {  
-  this.elProgressbar.style.width = Math.floor( (this.hp.current/this.hp.total) * 100 ) + '%';
-}
-
-function changeHP(count){
-  this.hp.current -= count;
-
-  const log = this === enemy ? generatorLog(this, character, count) : generatorLog(this, enemy, count);
-
-  if(this.hp.current <= 0){
-    this.hp.current = 0;
-    alert(this.name + 'Проиграл бой!');
-    disableAllAbilities();
-  }
-
-  this.renderHP();
-}
-
-function random(num) {
-  return Math.ceil(Math.random() * num);
+  initCharakterAbilities(player1);
 }
 
 function initCharakterAbilities(person) {
@@ -99,7 +51,9 @@ function initCharakterAbilities(person) {
         this.disabled = true;
         return;
       }
-      enemy.changeHP(random(person.abilities[i].damage));
+      player2.changeHP(random(person.abilities[i].damage), function(count) {
+        generatorLog(player1, player2, count)
+      });
 
     });  
 
